a17b74b44577328fe9dd0782921fc2e9b421d044
Reece Mercer <reecemercer@outlook.com>
2019-04-07 02:51:08 +0100

    Basic API read with axios headers setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const axios = require('axios');
-const stringify = require('json-stringify-safe')
+const express = require("express");
+const axios = require("axios");
+const mongoose = require('mongoose');
+const util = require('util')
 
 // intialise a new express app
 const app = express();
@@ -8,22 +9,32 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // load dotenv config files if not in a production environment
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 // initialise GitHub API details
-const username = process.env.GITHUB_USERNAME
+const username = process.env.GITHUB_USERNAME;
 const GITHUB_API_CLIENT_ID = process.env.GITHUB_API_CLIENT_ID;
 const GITHUB_API_CLIENT_SECRET = process.env.GITHUB_API_CLIENT_SECRET;
 
+const axiosRequestHeaders = {
+    params: {
+        client_id: GITHUB_API_CLIENT_ID,
+        client_secret: GITHUB_API_CLIENT_SECRET
+    }
+}
 
-app.get('/', (req, res, next) => {
-    axios.get(`https://api.github.com/rate_limit`)
-    .then(response => {
-        console.log(response);
-    })
+
+app.get("/myRepos", (req, res, next) => {
+  axios.get(`https://api.github.com/users/${username}/repos`, axiosRequestHeaders)
+  .then(response => response.data)
+  .then(response => {
+      for(let repo in response){
+          console.log(response[repo].name + " " + response[repo].stargazers_count)
+      }
+  })
+
 })
 
-
-app.listen(port, () => console.log("Server has started"))
+app.listen(port, () => console.log("Server has started"));
